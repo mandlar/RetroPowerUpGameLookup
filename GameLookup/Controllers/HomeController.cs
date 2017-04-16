ea0f589882988296b57375f80c9afdf063ead546
Bryan Denny <mandlar@gmail.com>
2017-04-16 15:34:16 -0400

    Updated Index view to use Search Model

--- GameLookup/Controllers/HomeController.cs
+++ GameLookup/Controllers/HomeController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GameLookup.Models;
 
 namespace GameLookup.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(SearchModel model)
         {
-            return View();
+            return View(model);
         }
     }
 }
