ed56062356b04322b499f79118c553c1c097b9ea
Bryan Denny <mandlar@gmail.com>
2017-04-19 00:31:54 -0400

    Added Web.Secrets.config file to hold IGDB API Key (no real key added, this file will be ignored for git)

--- GameLookup/Controllers/HomeController.cs
+++ GameLookup/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -7,7 +8,6 @@ using System.Web;
 using System.Web.Mvc;
 using GameLookup.Models;
 using Newtonsoft.Json;
-using unirest_net.http;
 
 namespace GameLookup.Controllers
 {
@@ -19,7 +19,7 @@ namespace GameLookup.Controllers
             {
                 using (HttpClient client = new HttpClient())
                 {
-                    client.DefaultRequestHeaders.TryAddWithoutValidation("X-Mashape-Key", "INSERT_API_KEY_HERE");
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("X-Mashape-Key", ConfigurationManager.AppSettings["IGDBApiKey"]);
                     client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
                     var json = client.GetStringAsync("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=*&limit=10&offset=0&search=" + model.Query);
                     
